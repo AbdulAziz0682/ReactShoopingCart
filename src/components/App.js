6f1a2f0b8a8fd6bd897cd8e31ef0e98d92d8016d
Abdul Aziz Khoso <60597318+AbdulAziz0682@users.noreply.github.com>
2021-05-17 17:28:04 +0500

    Set up basic router

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
 import React from "react";
-import { BrowserRouter, Switch } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./Navbar";
+import Shop from "./Shop";
+import Cart from "./Cart";
 
 export default function App() {
   return (
     <div className="w3-container">
       <BrowserRouter>
         <Navbar />
+        <Switch>
+          <Route exact path="/" component={Shop} />
+          <Route exact path="/cart" component={Cart} />
+        </Switch>
       </BrowserRouter>
     </div>
   );
