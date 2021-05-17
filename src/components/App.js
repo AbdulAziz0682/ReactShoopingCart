74589ec18f7dd331b7fd6b6edfb0164db4ab1b2c
Abdul Aziz Khoso <60597318+AbdulAziz0682@users.noreply.github.com>
2021-05-17 16:58:53 +0500

    Added Navbar

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="w3-container">
+      <h1 className="w3-header w3-deep-orange w3-center">Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
